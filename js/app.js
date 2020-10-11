27bf0c178fa36a819bbd7f300e50a4e2ac85e7df
Tomiwa Adelae <tommedia6001@gmail.com>
2020-10-10 22:05:19 -0200

    Fina; Commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const countdown = document.querySelector('.countdown');
+const launchDate = new Date('Dec 1, 2020, 13:00:00').getTime();
+
+// Update every sec
+const intvl = setInterval(() =>{
+   // Get Todays Date and Time (ms)
+   const now = new Date().getTime();
+
+   // Distance from Now to launch Date
+   const distance = launchDate - now;
+
+   // Calculations
+   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+   const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60 ))
+   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+   // Display Results
+   countdown.innerHTML = `
+      <div>${days}<span>Days</span></div>
+      <div>${hours}<span>Hours</span></div>
+      <div>${mins}<span>Minutes</span></div>
+      <div>${seconds}<span>Seconds</span></div>
+   `;
+
+   // If Launch Date Passed
+   if(distance < 0){
+      // Stop the Coundown
+      clearInterval(intvl);
+      // Style and Output Text
+      countdown.style.color = '#17a2b8';
+      countdown.innerHTML = 'Launched';
+   }
+}, 1000)
